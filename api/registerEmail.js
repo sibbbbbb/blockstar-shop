04d5bb55a02e0cd89b1092f9d1dc9ca374c4bd69
Enzo Correa <enzonicolascorrea@gmail.com>
2024-12-11 20:52:38 -0300

    Minor fix

--- api/registerEmail.js
+++ api/registerEmail.js
@@ -3,8 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_DB_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_DB_KEY
 
-console.log(supabaseUrl, supabaseAnonKey)
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export async function putEmail (email) {
