fe9a54001f28923bffe77b4e4042de2e96dac7a9
Enzo Correa <enzonicolascorrea@gmail.com>
2025-03-03 04:19:14 -0300

    fix: ux fixes

--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -48,35 +48,37 @@ export default function RootLayout({
 
   return (
     <div className="h-screen-dvh">
-      <NewsSlider
-        newsClassName="italic opacity-35 !h-7 text-[12px]"
-        containerClassName="!top-8 !border-y-[1px] !py-0 !h-7  "
-      />
+      {/* Menu */}
       <Image
-        className="absolute top-0 left-0 w-[150px] h-[150px] lg:w-[225px] lg:h-[225px] z-10"
+        className="fixed top-0 left-0 w-[150px] h-[150px] lg:w-[225px] lg:h-[225px] z-10"
         src={b1}
         alt="b-white"
       />
-      {!getHiddenLogo() && <Image
-        className="absolute bottom-0 right-0 w-[150px] h-[150px] lg:w-[225px] lg:h-[225px] -z-10"
+      <Image
+        className="fixed bottom-0 right-0 w-[150px] h-[150px] lg:w-[225px] lg:h-[225px] z-10"
         src={b2}
         alt="b-black"
-        />
-      }
-      <div className="flex flex-col h-screen-dvh absolute items-center justify-center bg-black-200 ml-5 md:ml-20 pt-10 space-y-14 z-50">
-        {icons.map((icon, index) => {
-          if (hiddenIcons.includes(icon.name)) return null;
-          return (
-            <button
-              key={index}
-              className="cursor-pointer"
-              onClick={icon.action}
-            >
-              <Image src={icon.icon} alt={icon.name} />
-            </button>
-          );
-        })}
+      />
+      <div className="fixed left-0 text-center h-full z-20">
+        <div className="flex flex-col items-start justify-center bg-black-200 space-y-14 ml-2 md:ml-8 pt-10 fixed top-[50%] transform translate-y-[-50%] p-5">
+          {icons.map((icon, index) => {
+            if (hiddenIcons.includes(icon.name)) return null;
+            return (
+              <button
+                key={index}
+                className="cursor-pointer w-64"
+                onClick={icon.action}
+              >
+                <Image src={icon.icon} alt={icon.name} />
+              </button>
+            );
+          })}
+        </div>
       </div>
+      <NewsSlider
+        newsClassName="italic opacity-35 !h-7 text-[12px]"
+        containerClassName="!top-8 !border-y-[1px] !py-0 !h-7"
+      />
       <div className="z-50">{children}</div>
     </div>
   );
